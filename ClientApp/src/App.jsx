0431495f8b4edee00485287435af5d0c2ebfbd2e
Isaac Rendon <isaacrendon7@gmail.com>
2020-10-08 09:48:44 -0400

    Started building static html for home page

--- ClientApp/src/App.jsx
+++ ClientApp/src/App.jsx
@@ -1,8 +1,22 @@
 import React from 'react'
 import './custom.scss'
+import { Header } from './components/Header'
+import { Box } from './components/Box'
 
 export function App() {
   return (
-    <div className="d-flex justify-content-center display-2">Hello, World!</div>
+    <main>
+      <Header />
+      <section className="boxContainer">
+        <Box />
+        <Box />
+        <Box />
+        <Box />
+        <Box />
+        <Box />
+        <Box />
+        <Box />
+      </section>
+    </main>
   )
 }
---
+++ ClientApp/src/components/Header.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+export function Header() {
+  return (
+    <div>
+      <header>
+        <h1>On Repeat</h1>
+      </header>
+      <nav>
+        <ul className="navBar">
+          <li>Home</li>
+          <li>My Playlists</li>
+          <li>My Charts</li>
+        </ul>
+      </nav>
+    </div>
+  )
+}
