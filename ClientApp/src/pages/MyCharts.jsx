f121bdff7f4c811c7dbdd36eeb8fcfbdb1f48a33
Isaac Rendon <isaacrendon7@gmail.com>
2020-10-08 10:48:24 -0400

    hard coded some example data to charts

--- ClientApp/src/pages/MyCharts.jsx
+++ ClientApp/src/pages/MyCharts.jsx
@@ -4,9 +4,99 @@ export function MyCharts() {
   return (
     <div>
       <section className="charts">
-        <article className="topArtists"></article>
+        <article className="topArtists">
+          <ul>
+            <li className="top">
+              <ul className="topList">
+                <li>1. Kanye West</li>
+                <li>2. Juls</li>
+                <li>3. Kid Cudi</li>
+                <li>4. A$AP Rocky</li>
+                <li>5. Tyler the Creator</li>
+                <li>6. Mac Miller</li>
+                <li>7. Jay Cole</li>
+                <li>8. JID</li>
+                <li>9. Migos</li>
+                <li>10. WizKid</li>
+              </ul>
+            </li>
+            <li className="top">
+              <ul className="topList">
+                <li>1. Kanye West</li>
+                <li>2. Juls</li>
+                <li>3. Kid Cudi</li>
+                <li>4. A$AP Rocky</li>
+                <li>5. Tyler the Creator</li>
+                <li>6. Mac Miller</li>
+                <li>7. Jay Cole</li>
+                <li>8. JID</li>
+                <li>9. Migos</li>
+                <li>10. WizKid</li>
+              </ul>
+            </li>
+            <li className="top">
+              <ul className="topList">
+                <li>1. Kanye West</li>
+                <li>2. Juls</li>
+                <li>3. Kid Cudi</li>
+                <li>4. A$AP Rocky</li>
+                <li>5. Tyler the Creator</li>
+                <li>6. Mac Miller</li>
+                <li>7. Jay Cole</li>
+                <li>8. JID</li>
+                <li>9. Migos</li>
+                <li>10. WizKid</li>
+              </ul>
+            </li>
+          </ul>
+        </article>
 
-        <article className="topSongs"></article>
+        <article className="topSongs">
+          <ul>
+            <li className="top">
+              <ul className="topList">
+                <li>1. Kanye West</li>
+                <li>2. Juls</li>
+                <li>3. Kid Cudi</li>
+                <li>4. A$AP Rocky</li>
+                <li>5. Tyler the Creator</li>
+                <li>6. Mac Miller</li>
+                <li>7. Jay Cole</li>
+                <li>8. JID</li>
+                <li>9. Migos</li>
+                <li>10. WizKid</li>
+              </ul>
+            </li>
+            <li className="top">
+              <ul className="topList">
+                <li>1. Kanye West</li>
+                <li>2. Juls</li>
+                <li>3. Kid Cudi</li>
+                <li>4. A$AP Rocky</li>
+                <li>5. Tyler the Creator</li>
+                <li>6. Mac Miller</li>
+                <li>7. Jay Cole</li>
+                <li>8. JID</li>
+                <li>9. Migos</li>
+                <li>10. WizKid</li>
+              </ul>
+            </li>
+            <li className="top">
+              <ul className="topList">
+                <li>1. Kanye West</li>
+                <li>2. Juls</li>
+                <li>3. Kid Cudi</li>
+                <li>4. A$AP Rocky</li>
+                <li>5. Tyler the Creator</li>
+                <li>6. Mac Miller</li>
+                <li>7. Jay Cole</li>
+                <li>8. JID</li>
+                <li>9. Migos</li>
+                <li>10. WizKid</li>
+              </ul>
+            </li>
+          </ul>
+        </article>
       </section>
     </div>
   )
