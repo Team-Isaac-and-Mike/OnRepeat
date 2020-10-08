810123f073941d0dd52d8003d7c59c45dfd91ad8
Isaac Rendon <isaacrendon7@gmail.com>
2020-10-08 12:35:18 -0400

    Started building rough example of playlist creator

--- ClientApp/src/pages/CreateAPlaylist.jsx
+++ ClientApp/src/pages/CreateAPlaylist.jsx
@@ -3,7 +3,32 @@ import React from 'react'
 export function CreateAPlaylist() {
   return (
     <div>
-      <section>test</section>
+      <section className="creatorContainer">
+        <article className="creator">
+          <p>
+            <label>Name your playlist:</label>
+            <input type="text"></input>
+          </p>
+          <p>
+            <label>What kind of genre is this playlist:</label>
+            <select name="genre">
+              <option value="Rap">Rap</option>
+              <option value="Pop">Pop</option>
+              <option value="Jazz">Jazz</option>
+            </select>
+          </p>
+          <p>
+            <label>What kind of playlist is this:</label>
+            <select name="gerne">
+              <option value="Upbeat">Upbeat</option>S
+              <option value="Instrumental">Instrumental</option>
+              <option value="Live">Live</option>
+              <option value="Danceable">Danceable</option>
+            </select>
+          </p>
+          <button>Create my playlist</button>
+        </article>
+      </section>
     </div>
   )
 }
