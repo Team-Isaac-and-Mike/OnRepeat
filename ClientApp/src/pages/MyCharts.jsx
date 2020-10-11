c7abef624d1ff472369e646b6279f758b986c8c6
Michael Rushing <mikerush1023@gmail.com>
2020-10-11 17:35:16 -0400

    Updated scss more

--- ClientApp/src/pages/MyCharts.jsx
+++ ClientApp/src/pages/MyCharts.jsx
@@ -5,8 +5,21 @@ export function MyCharts() {
     <div>
       <section className="charts">
         <article className="topArtists">
-          <ul>
-            <li className="top">
+          
+              <ul className="topList">
+                <li>1. Kanye West</li>
+                <li>2. Juls</li>
+                <li>3. Kid Cudi</li>
+                <li>4. A$AP Rocky</li>
+                <li>5. Tyler the Creator</li>
+                <li>6. Mac Miller</li>
+                <li>7. Jay Cole</li>
+                <li>8. JID</li>
+                <li>9. Migos</li>
+                <li>10. WizKid</li>
+                </ul>
+              
+            
               <ul className="topList">
                 <li>1. Kanye West</li>
                 <li>2. Juls</li>
@@ -19,8 +32,8 @@ export function MyCharts() {
                 <li>9. Migos</li>
                 <li>10. WizKid</li>
               </ul>
-            </li>
-            <li className="top">
+
+
               <ul className="topList">
                 <li>1. Kanye West</li>
                 <li>2. Juls</li>
@@ -33,27 +46,12 @@ export function MyCharts() {
                 <li>9. Migos</li>
                 <li>10. WizKid</li>
               </ul>
-            </li>
-            <li className="top">
-              <ul className="topList">
-                <li>1. Kanye West</li>
-                <li>2. Juls</li>
-                <li>3. Kid Cudi</li>
-                <li>4. A$AP Rocky</li>
-                <li>5. Tyler the Creator</li>
-                <li>6. Mac Miller</li>
-                <li>7. Jay Cole</li>
-                <li>8. JID</li>
-                <li>9. Migos</li>
-                <li>10. WizKid</li>
-              </ul>
-            </li>
-          </ul>
+
+
         </article>
 
         <article className="topSongs">
-          <ul>
-            <li className="top">
+          
               <ul className="topList">
                 <li>1. Kanye West</li>
                 <li>2. Juls</li>
@@ -66,8 +64,8 @@ export function MyCharts() {
                 <li>9. Migos</li>
                 <li>10. WizKid</li>
               </ul>
-            </li>
-            <li className="top">
+
+            
               <ul className="topList">
                 <li>1. Kanye West</li>
                 <li>2. Juls</li>
@@ -80,8 +78,8 @@ export function MyCharts() {
                 <li>9. Migos</li>
                 <li>10. WizKid</li>
               </ul>
-            </li>
-            <li className="top">
+
+            
               <ul className="topList">
                 <li>1. Kanye West</li>
                 <li>2. Juls</li>
@@ -94,8 +92,8 @@ export function MyCharts() {
                 <li>9. Migos</li>
                 <li>10. WizKid</li>
               </ul>
-            </li>
-          </ul>
+            
+          
         </article>
       </section>
     </div>
