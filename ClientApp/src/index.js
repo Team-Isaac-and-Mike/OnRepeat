22188752e499243a3c06e8f0935b6a86dc5cc3b3
Isaac Rendon <isaacrendon7@gmail.com>
2020-10-13 15:19:27 -0400

    Hooked up Auth0

--- ClientApp/src/index.js
+++ ClientApp/src/index.js
@@ -4,10 +4,13 @@ import ReactDOM from 'react-dom'
 import { BrowserRouter } from 'react-router-dom'
 import { App } from './App'
 import { Auth0Provider } from '@auth0/auth0-react'
+// import env from 'process-env'
 
-const domain = process.env.REACT_APP_AUTH0_DOMAIN
-const clientId = process.env.REACT_APP_AUTH0_CLIENT_ID
-
+// const domain = process.env.REACT_APP_AUTH0_DOMAIN
+// const clientId = process.env.REACT_APP_AUTH0_CLIENT_ID
+const domain = 'dev-ta9ujpoj.us.auth0.com'
+const clientId = 'oMfVSuNASvvOKr1Wl6OVU11fW0Li2415'
+// console.log(process.env)
 const baseUrl = document.getElementsByTagName('base')[0].getAttribute('href')
 const rootElement = document.getElementById('root')
 
