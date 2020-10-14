90ae8f90065e88132d36079c1555f5337f100a67
Isaac Rendon <isaacrendon7@gmail.com>
2020-10-14 15:48:46 -0400

    Connected json with hooks

--- ClientApp/src/components/Box.jsx
+++ ClientApp/src/components/Box.jsx
@@ -1,9 +1,22 @@
 import React from 'react'
+import playlists from '../playlists.json'
 
 export function Box() {
   return (
     <div>
-      <article className="boxArticle">Content goes here</article>
+        {playlists.data.map((playlists)=>(
+          <article className="boxArticle">
+          <figure>
+          <img
+            src={playlists.image}
+            />
+          <figcaption>
+            {playlists.name}
+          </figcaption>
+        </figure>
+      </article>
+        ))}
+        
     </div>
   )
 }
