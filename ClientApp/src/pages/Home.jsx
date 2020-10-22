ab28316d21cc8976b3b3dadd2d3f048cbd43b6e3
Isaac Rendon <isaacrendon7@gmail.com>
2020-10-22 13:42:38 -0400

    test

--- ClientApp/src/pages/Home.jsx
+++ ClientApp/src/pages/Home.jsx
@@ -2,24 +2,24 @@ import React, { useEffect, useState } from 'react'
 import { PlayListsBox } from '../components/PlaylistBox'
 
 export function Home() {
-  const [userInfo, setUserInfo] = useState([])
-  const accessToken = localStorage.getItem('SpotifyAccessToken')
+  //   const [userInfo, setUserInfo] = useState([])
+  //   const accessToken = localStorage.getItem('SpotifyAccessToken')
 
-  async function fetchUserInfo() {
-    const response = await fetch(`https://api.spotify.com/v1/me`, {
-      headers: {
-        Authorization: `Bearer ${accessToken}`,
-      },
-    })
-    const json = await response.json()
+  //   async function fetchUserInfo() {
+  //     const response = await fetch(`https://api.spotify.com/v1/me`, {
+  //       headers: {
+  //         Authorization: `Bearer ${accessToken}`,
+  //       },
+  //     })
+  //     const json = await response.json()
 
-    setUserInfo(json)
-  }
+  //     setUserInfo(json)
+  //   }
 
-  useEffect(() => {
-    fetchUserInfo()
-  }, [])
-  console.log(userInfo.display_name)
+  //   useEffect(() => {
+  //     fetchUserInfo()
+  //   }, [])
+  //   // console.log(userInfo.display_name)
 
   return (
     <div className="home">
